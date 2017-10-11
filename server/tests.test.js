e87ebd9b6ce4de95658b953b12d41c1c53eea86d
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-11 14:32:46 +0200

    added a test

--- server/tests.test.js
+++ server/tests.test.js
@@ -1,7 +1,11 @@
 import { expect } from 'chai';
 
 describe('Server tests are working', () => {
-  it('does math ', () => {
+  it('does math', () => {
     expect(1 + 1).to.equal(2);
+  });
+
+  it('does more math', () => {
+    expect(1 - 1).to.equal(0);
   });
 });
