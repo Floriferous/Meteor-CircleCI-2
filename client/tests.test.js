154d2c2436d8d781c0f4cd2ebb11b35aab0968b1
Florian Bienefelt <florian.bienefelt@gmail.com>
2017-10-11 14:30:26 +0200

    added circle CI config file

--- client/tests.test.js
+++ client/tests.test.js
@@ -4,4 +4,8 @@ describe('Client tests are working', () => {
   it('does string manipulation', () => {
     expect('a' + 1).to.equal('a1');
   });
+
+  it('does string manipulation 2', () => {
+    expect('a' + 2).to.equal('a2');
+  });
 });
--- server/tests.test.js
+++ server/tests.test.js
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 
 describe('Server tests are working', () => {
-  it('does math', () => {
+  it('does math ', () => {
     expect(1 + 1).to.equal(2);
   });
 });
