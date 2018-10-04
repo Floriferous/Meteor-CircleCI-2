e51ee7369751b2b3723433cc5d6b25a3f759e6b6
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-04 16:35:20 +0200

    Use correct tag name

--- install-meteor.sh
+++ install-meteor.sh
@@ -5,7 +5,7 @@ METEOR_VERSION=1.8-rc.16
 cd ~
 git clone https://github.com/meteor/meteor.git --recursive ./bin
 cd bin
-git checkout $METEOR_VERSION
+git checkout release/METEOR@$METEOR_VERSION
 ./meteor --version
 
 # mkdir ~/bin
