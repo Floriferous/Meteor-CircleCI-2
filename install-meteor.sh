7ca8ea4c77f249bf7507d47a12bd023252dcf15b
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-10-04 16:30:27 +0200

    Clone directly inside bin

--- install-meteor.sh
+++ install-meteor.sh
@@ -3,10 +3,10 @@
 METEOR_VERSION=1.8-rc.17
 
 cd ~
-git clone https://github.com/meteor/meteor.git --recursive
-cd meteor
+git clone https://github.com/meteor/meteor.git --recursive ./bin
+cd bin
 git checkout $METEOR_VERSION
 ./meteor --version
 
-mkdir ~/bin
-ln meteor ~/bin/meteor
+# mkdir ~/bin
+# ln meteor ~/bin/meteor
